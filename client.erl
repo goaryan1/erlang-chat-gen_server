e8bad952b2ef64ca96d1c96b254442929d0f734d
Jugal-GreyOrange <158149871+Jugal-GreyOrange@users.noreply.github.com>
2024-02-16 15:45:43 +0530

    This is the base code which just broadcast messages
    Now in branch all the features will be added to both server and client files

--- client.erl
+++ client.erl
@@ -1,11 +1,16 @@
 -module(client).
  
--export([start/0, listen_loop/1, send_message/0]).
+-export([start/0, listen_loop/1, start_helper/1, send_message/0]).
 -record(client_status, {name, serverSocket, startPid, serverNode}).
 % -define(SERVER, server).
  
+
 start() ->
-    io:format("?MODULE = ~p~n", [?MODULE]),
+    ClientStatus = #client_status{startPid = self()},
+    SpawnedPid = spawn(fun() -> start_helper(ClientStatus) end),
+    put(spawnedPid, SpawnedPid).
+
+start_helper(ClientStatus) ->
     io:format("Connecting to server...~n"),
     {ok, Socket} = gen_tcp:connect('localhost', 9991, [binary, {active, true}]),
     gen_tcp:recv(Socket, 0),
@@ -14,11 +19,10 @@ start() ->
             Data = erlang:binary_to_term(BinaryData),
             {success ,ServerNode, ClientName} = Data,
             io:format("Successfully joined with Username : ~p~n",[ClientName]),
-            ClientStatus = #client_status{name = ClientName, serverSocket = Socket, startPid = self(), serverNode = ServerNode},
-            SpawnedPid = spawn(fun() -> listen_loop(ClientStatus) end),
-            put(spawnedPid, SpawnedPid),
-            % listen_loop(ClientStatus),
-            ok;
+            ClientStatus1 = ClientStatus#client_status{name = ClientName, serverSocket = Socket, serverNode = ServerNode},
+            gen_server:call({server, ServerNode}, {username, ClientName}),
+            
+            listen_loop(ClientStatus1);
         {tcp_closed, Socket} ->
             io:format("Connection closed~n")
     end.
@@ -27,10 +31,13 @@ listen_loop(ClientStatus) ->
     Socket = ClientStatus#client_status.serverSocket,
     StartPid = ClientStatus#client_status.startPid,
     ServerNode = ClientStatus#client_status.serverNode,
+    gen_tcp:recv(Socket, 0),
     receive
         {tcp, Socket, BinaryData} ->
             Data = binary_to_term(BinaryData),
             case Data of
+                {message, SenderName, Message} ->
+                    io:format("~p : ~p~n", [SenderName, Message]);
                 _ ->
                     io:format("Undefined message received~n")
             end;
@@ -41,16 +48,12 @@ listen_loop(ClientStatus) ->
             case Data of
                 {message, Message} ->
                     Request = {message, Message},
-                    % ServerNode = ,
-                    Response = gen_server:call({server, ServerNode}, Request),
-                    io:format("~p~n", [Response]),
+                    gen_server:call({server, ServerNode}, Request),
                     ok;
-                    % gen_tcp:send(Socket, BinaryData);
                 _ ->
                     io:format("Undefined internal message received~n")
             end
     end,
- 
     listen_loop(ClientStatus).
  
 send_message() ->
